704607ecf7e4fc9c0b7632ab649484e34fae3fa8
Nucizzz <jnucibella@gmail.com>
2025-11-08 16:33:51 +0100

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Nav from "./components/nav.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import Scan from "./pages/Scan.jsx";
-import Ops from "./pages/Ops.jsx";
-import NewProduct from "./pages/NewProduct.jsx";
-import Settings from "./pages/Setting.jsx";
+import Dashboard from "./pages/dashboard.jsx";
+import Scan from "./pages/scan.jsx";
+import Ops from "./pages/ops.jsx";
+import NewProduct from "./pages/newProduct.jsx";
+import Settings from "./pages/setting.jsx";
 
 export default function App() {
   return (
